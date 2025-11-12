0497f699a41e0e897c731a93aaa13c6cf748b8bb
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2025-11-12 09:03:43 +0100

    gombok változása/adatbázis táblák létrhozása adatokkal

--- db/FotStats.sql
+++ db/FotStats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Nov 12. 08:16
+-- Létrehozás ideje: 2025. Nov 12. 09:02
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -31,8 +31,8 @@ CREATE TABLE `csapatok` (
   `id` int(11) NOT NULL,
   `nev` varchar(100) NOT NULL,
   `orszag` varchar(100) DEFAULT NULL,
-  `liga` varchar(100) DEFAULT NULL,
   `tipus` enum('klub','valogatott') DEFAULT 'klub',
+  `liga_id` int(11) DEFAULT NULL,
   `helyezes` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -40,47 +40,47 @@ CREATE TABLE `csapatok` (
 -- A tábla adatainak kiíratása `csapatok`
 --
 
-INSERT INTO `csapatok` (`id`, `nev`, `orszag`, `liga`, `tipus`, `helyezes`) VALUES
-(1, 'Manchester City', 'Anglia', 'Premier League', 'klub', 1),
-(2, 'Arsenal', 'Anglia', 'Premier League', 'klub', 2),
-(3, 'Liverpool', 'Anglia', 'Premier League', 'klub', 3),
-(4, 'Aston Villa', 'Anglia', 'Premier League', 'klub', 4),
-(5, 'Tottenham Hotspur', 'Anglia', 'Premier League', 'klub', 5),
-(6, 'Real Madrid', 'Spanyolország', 'La Liga', 'klub', 1),
-(7, 'Barcelona', 'Spanyolország', 'La Liga', 'klub', 2),
-(8, 'Atletico Madrid', 'Spanyolország', 'La Liga', 'klub', 3),
-(9, 'Girona', 'Spanyolország', 'La Liga', 'klub', 4),
-(10, 'Real Sociedad', 'Spanyolország', 'La Liga', 'klub', 5),
-(11, 'Bayer Leverkusen', 'Németország', 'Bundesliga', 'klub', 1),
-(12, 'Bayern München', 'Németország', 'Bundesliga', 'klub', 2),
-(13, 'Stuttgart', 'Németország', 'Bundesliga', 'klub', 3),
-(14, 'RB Leipzig', 'Németország', 'Bundesliga', 'klub', 4),
-(15, 'Borussia Dortmund', 'Németország', 'Bundesliga', 'klub', 5),
-(16, 'Inter', 'Olaszország', 'Serie A', 'klub', 1),
-(17, 'AC Milan', 'Olaszország', 'Serie A', 'klub', 2),
-(18, 'Juventus', 'Olaszország', 'Serie A', 'klub', 3),
-(19, 'Napoli', 'Olaszország', 'Serie A', 'klub', 4),
-(20, 'Atalanta', 'Olaszország', 'Serie A', 'klub', 5),
-(21, 'Paris Saint-Germain', 'Franciaország', 'Ligue 1', 'klub', 1),
-(22, 'Monaco', 'Franciaország', 'Ligue 1', 'klub', 2),
-(23, 'Lille', 'Franciaország', 'Ligue 1', 'klub', 3),
-(24, 'Marseille', 'Franciaország', 'Ligue 1', 'klub', 4),
-(25, 'Nice', 'Franciaország', 'Ligue 1', 'klub', 5),
-(26, 'Real Madrid', 'Spanyolország', 'Bajnokok Ligája', 'klub', 1),
-(27, 'Manchester City', 'Anglia', 'Bajnokok Ligája', 'klub', 2),
-(28, 'Bayern München', 'Németország', 'Bajnokok Ligája', 'klub', 3),
-(29, 'Paris Saint-Germain', 'Franciaország', 'Bajnokok Ligája', 'klub', 4),
-(30, 'Inter', 'Olaszország', 'Bajnokok Ligája', 'klub', 5),
-(31, 'Fiorentina', 'Olaszország', 'Európa Konferencia Liga', 'klub', 1),
-(32, 'Aston Villa', 'Anglia', 'Európa Konferencia Liga', 'klub', 2),
-(33, 'Club Brugge', 'Belgium', 'Európa Konferencia Liga', 'klub', 3),
-(34, 'Lille', 'Franciaország', 'Európa Konferencia Liga', 'klub', 4),
-(35, 'PAOK', 'Görögország', 'Európa Konferencia Liga', 'klub', 5),
-(36, 'Franciaország', 'Franciaország', 'VB-selejtező', 'valogatott', 1),
-(37, 'Anglia', 'Anglia', 'VB-selejtező', 'valogatott', 2),
-(38, 'Portugália', 'Portugália', 'VB-selejtező', 'valogatott', 3),
-(39, 'Spanyolország', 'Spanyolország', 'VB-selejtező', 'valogatott', 4),
-(40, 'Magyarország', 'Magyarország', 'VB-selejtező', 'valogatott', 5);
+INSERT INTO `csapatok` (`id`, `nev`, `orszag`, `tipus`, `liga_id`, `helyezes`) VALUES
+(1, 'Manchester City', 'Anglia', 'klub', 1, 1),
+(2, 'Arsenal', 'Anglia', 'klub', 1, 2),
+(3, 'Liverpool', 'Anglia', 'klub', 1, 3),
+(4, 'Aston Villa', 'Anglia', 'klub', 1, 4),
+(5, 'Tottenham Hotspur', 'Anglia', 'klub', 1, 5),
+(6, 'Real Madrid', 'Spanyolország', 'klub', 2, 1),
+(7, 'Barcelona', 'Spanyolország', 'klub', 2, 2),
+(8, 'Atletico Madrid', 'Spanyolország', 'klub', 2, 3),
+(9, 'Girona', 'Spanyolország', 'klub', 2, 4),
+(10, 'Real Sociedad', 'Spanyolország', 'klub', 2, 5),
+(11, 'Bayer Leverkusen', 'Németország', 'klub', 3, 1),
+(12, 'Bayern München', 'Németország', 'klub', 3, 2),
+(13, 'Stuttgart', 'Németország', 'klub', 3, 3),
+(14, 'RB Leipzig', 'Németország', 'klub', 3, 4),
+(15, 'Borussia Dortmund', 'Németország', 'klub', 3, 5),
+(16, 'Inter', 'Olaszország', 'klub', 4, 1),
+(17, 'AC Milan', 'Olaszország', 'klub', 4, 2),
+(18, 'Juventus', 'Olaszország', 'klub', 4, 3),
+(19, 'Napoli', 'Olaszország', 'klub', 4, 4),
+(20, 'Atalanta', 'Olaszország', 'klub', 4, 5),
+(21, 'Paris Saint-Germain', 'Franciaország', 'klub', 5, 1),
+(22, 'Monaco', 'Franciaország', 'klub', 5, 2),
+(23, 'Lille', 'Franciaország', 'klub', 5, 3),
+(24, 'Marseille', 'Franciaország', 'klub', 5, 4),
+(25, 'Nice', 'Franciaország', 'klub', 5, 5),
+(26, 'Real Madrid', 'Spanyolország', 'klub', 6, 1),
+(27, 'Manchester City', 'Anglia', 'klub', 6, 2),
+(28, 'Bayern München', 'Németország', 'klub', 6, 3),
+(29, 'Paris Saint-Germain', 'Franciaország', 'klub', 6, 4),
+(30, 'Inter', 'Olaszország', 'klub', 6, 5),
+(31, 'Fiorentina', 'Olaszország', 'klub', 7, 1),
+(32, 'Aston Villa', 'Anglia', 'klub', 7, 2),
+(33, 'Club Brugge', 'Belgium', 'klub', 7, 3),
+(34, 'Lille', 'Franciaország', 'klub', 7, 4),
+(35, 'PAOK', 'Görögország', 'klub', 7, 5),
+(36, 'Franciaország', 'Franciaország', 'valogatott', 8, 1),
+(37, 'Anglia', 'Anglia', 'valogatott', 8, 2),
+(38, 'Portugália', 'Portugália', 'valogatott', 8, 3),
+(39, 'Spanyolország', 'Spanyolország', 'valogatott', 8, 4),
+(40, 'Magyarország', 'Magyarország', 'valogatott', 8, 5);
 
 -- --------------------------------------------------------
 
@@ -182,6 +182,83 @@ INSERT INTO `jatekosok` (`id`, `nev`, `poszt`, `nemzetiseg`, `csapat_id`) VALUES
 (79, 'Dominik Szoboszlai', 'Középpályás', 'Magyarország', 40),
 (80, 'Roland Sallai', 'Szélső', 'Magyarország', 40);
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `ligak`
+--
+
+CREATE TABLE `ligak` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- A tábla adatainak kiíratása `ligak`
+--
+
+INSERT INTO `ligak` (`id`, `nev`) VALUES
+(1, 'Premier League'),
+(2, 'La Liga'),
+(3, 'Bundesliga'),
+(4, 'Serie A'),
+(5, 'Ligue 1'),
+(6, 'Bajnokok Ligája'),
+(7, 'Európa Konferencia Liga'),
+(8, 'VB-selejtező');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `meccsek`
+--
+
+CREATE TABLE `meccsek` (
+  `id` int(11) NOT NULL,
+  `csapat1_id` int(11) NOT NULL,
+  `csapat2_id` int(11) NOT NULL,
+  `datum` date DEFAULT NULL,
+  `liga_id` int(11) NOT NULL,
+  `eredmeny` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- A tábla adatainak kiíratása `meccsek`
+--
+
+INSERT INTO `meccsek` (`id`, `csapat1_id`, `csapat2_id`, `datum`, `liga_id`, `eredmeny`) VALUES
+(1, 1, 2, '2025-08-15', 1, '2-1'),
+(2, 3, 4, '2025-08-16', 1, '1-1'),
+(3, 6, 7, '2025-08-17', 2, '3-2'),
+(4, 8, 9, '2025-09-04', 2, NULL),
+(5, 26, 27, '2025-09-10', 6, '3-0'),
+(6, 31, 32, '2025-09-15', 7, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'user',
+  `reg_date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `reg_date`) VALUES
+(1, 'admin', 'admin@foci.hu', 'admin123', 'admin', '2025-11-12 08:25:04'),
+(2, 'richard', 'richard@foci.hu', 'jelszo123', 'user', '2025-11-12 08:25:04'),
+(3, 'balazs', 'balazs@foci.hu', 'titok456', 'user', '2025-11-12 08:25:04'),
+(4, 'anna', 'anna@foci.hu', 'pass789', 'user', '2025-11-12 08:25:04');
+
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -190,7 +267,8 @@ INSERT INTO `jatekosok` (`id`, `nev`, `poszt`, `nemzetiseg`, `csapat_id`) VALUES
 -- A tábla indexei `csapatok`
 --
 ALTER TABLE `csapatok`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_csapat_liga` (`liga_id`);
 
 --
 -- A tábla indexei `jatekosok`
@@ -198,6 +276,29 @@ ALTER TABLE `csapatok`
 ALTER TABLE `jatekosok`
   ADD PRIMARY KEY (`id`),
   ADD KEY `csapat_id` (`csapat_id`);
+
+--
+-- A tábla indexei `ligak`
+--
+ALTER TABLE `ligak`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `meccsek`
+--
+ALTER TABLE `meccsek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `csapat1_id` (`csapat1_id`),
+  ADD KEY `csapat2_id` (`csapat2_id`),
+  ADD KEY `liga_id` (`liga_id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -216,14 +317,46 @@ ALTER TABLE `jatekosok`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
+-- AUTO_INCREMENT a táblához `ligak`
+--
+ALTER TABLE `ligak`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT a táblához `meccsek`
+--
+ALTER TABLE `meccsek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- Megkötések a kiírt táblákhoz
 --
+
+--
+-- Megkötések a táblához `csapatok`
+--
+ALTER TABLE `csapatok`
+  ADD CONSTRAINT `fk_csapat_liga` FOREIGN KEY (`liga_id`) REFERENCES `ligak` (`id`) ON DELETE SET NULL;
 
 --
 -- Megkötések a táblához `jatekosok`
 --
 ALTER TABLE `jatekosok`
   ADD CONSTRAINT `jatekosok_ibfk_1` FOREIGN KEY (`csapat_id`) REFERENCES `csapatok` (`id`) ON DELETE CASCADE;
+
+--
+-- Megkötések a táblához `meccsek`
+--
+ALTER TABLE `meccsek`
+  ADD CONSTRAINT `meccsek_ibfk_1` FOREIGN KEY (`csapat1_id`) REFERENCES `csapatok` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `meccsek_ibfk_2` FOREIGN KEY (`csapat2_id`) REFERENCES `csapatok` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `meccsek_ibfk_3` FOREIGN KEY (`liga_id`) REFERENCES `ligak` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
