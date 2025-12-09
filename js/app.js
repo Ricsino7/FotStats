d206670b0e25e4853c2aff7d4bdbfb9cc1b73a2c
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-12-09 13:27:27 +0100

    select

--- js/app.js
+++ js/app.js
@@ -200,15 +200,9 @@
             // Így a HTML-ben az ng-repeat automatikusan frissíti a táblázatot
             
           $scope.Ligak = [...new Set(response.data.map(x => x.Liga))];
-          $scope.data=response.data.filter(x=> x.Liga=="Premier League");
-          
-          $scope.tabella_kivalasztas = () => {
-            console.log("Kiválasztva:", $scope.kivalasztott_tabella);
-            $scope.filteredTabella = response.data.filter(x=> x.Liga==$scope.kivalasztott_tabella)
-            $scope.data=$scope.filteredTabella;
-            
-          };
-
+          $scope.data=response.data;
+          $scope.kivalasztott_tabella = "Premier League";
+          $scope.$applyAsync();
         })
         // Ha valami hiba történik a lekérés során, ezt a függvényt hívja meg
         .catch(function(error) {
@@ -218,7 +212,4 @@
         });
 
 }]);
-
-
-
 })(window, angular);
