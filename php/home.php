7d256145f4312e0e560ee49150a96f698fd22f8a
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2025-12-09 07:56:36 +0100

    Php frissitése

--- php/home.php
+++ php/home.php
@@ -2,7 +2,9 @@
 require_once('../../common/php/environment.php');
 
 $db = new Database('fotstats');
-$rows = $db->execute("SELECT * FROM `csapatok`");
+$rows = $db->execute("SELECT `id`, `nev`, `orszag`, 
+                `tipus`, `liga_id`, `helyezes`
+         FROM `csapatok`");
 $db = null;
 
 echo json_encode($rows ?: []);
--- php/news.php
+++ php/news.php
@@ -2,7 +2,9 @@
 require_once('../../common/php/environment.php');
 
 $db = new Database('fotstats');
-$rows = $db->execute("SELECT * FROM `news`");
+$rows = $db->execute("SELECT `id`, `cim`, `datum`, 
+                `forras`, `osszefoglalo`
+         FROM `news`");
 $db = null;
 
 echo json_encode($rows ?: []);
