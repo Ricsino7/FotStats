fc82d5918f65be514602977c1921379fa8650d4c
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2026-01-06 07:58:17 +0100

    Uj wpf

--- FotStats_Wpf/FotStats/FotStats/Meccsek.xaml.cs
+++ FotStats_Wpf/FotStats/FotStats/Meccsek.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FotStats
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Meccsek.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Meccsek : Window
     {
-        public MainWindow()
+        public Meccsek()
         {
             InitializeComponent();
         }
--- FotStats_Wpf/FotStats/FotStats/Properties/AssemblyInfo.cs
+++ FotStats_Wpf/FotStats/FotStats/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FotStats")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
