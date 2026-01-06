1ca7fa3532788f1fc1ab70180269b4dd3728216f
KulcsarTamas1 <kulcsar.tamas-2021@keri.mako.hu>
2026-01-06 10:59:12 +0100

    language

--- js/app.js
+++ js/app.js
@@ -76,11 +76,59 @@
         }
       ])
 
+
     //Application run
     .run([
       '$rootScope',
       ($rootScope) => {
         $rootScope.darkMode = "dark";
+        $rootScope.languageTranslations = {
+  hu: "Magyar 🇭🇺",
+  en: "English 🇺🇸",
+  de: "Deutsch 🇩🇪"
+};
+
+// get languages
+$rootScope.getLanguages = function () {
+  fetch('./php/getLanguages.php')
+    .then(res => res.json())
+    .then(res => {
+      if (res.error) {
+        console.error(res.error);
+        return;
+      }
+
+      // csak a szükséges nyelvek
+      $rootScope.languages = res.data
+        .filter(l => ['hu', 'en', 'de'].includes(l.language))
+        .map(l => ({
+          ...l,
+          data: JSON.parse(l.data)
+        }));
+
+      // alapértelmezett: magyar
+      const defaultLang = $rootScope.languages.find(l => l.language === 'hu');
+      if (defaultLang) {
+        $rootScope.currentLang = defaultLang.data;
+        $rootScope.currentFlag = defaultLang.language;
+      }
+
+      $rootScope.$applyAsync();
+    })
+    .catch(err => console.error(err));
+};
+
+// run
+$rootScope.getLanguages();
+
+$rootScope.selectedLanguage = function (langData) {
+  const lang = $rootScope.languages.find(l => l.data === langData);
+  if (lang) {
+    $rootScope.currentLang = lang.data;
+    $rootScope.currentFlag = lang.language;
+  }
+};
+
       }
     ])
  
