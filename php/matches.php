67e177dd350a8d44c9155c167bd54b2ac2478c55
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2026-01-07 14:19:38 +0100

    forditások(Home/Matches/Footer

--- php/matches.php
+++ php/matches.php
@@ -5,10 +5,10 @@ $db = new Database('fotstats');
 
 $rows = $db->execute("
     SELECT 
-        c1.nev AS Hazai,
-        c2.nev AS Vendég,
-        m.datum AS Dátum,
-        l.nev AS Liga
+        c1.nev AS hazai,
+        c2.nev AS vendeg,
+        m.datum AS datum,
+        l.nev AS liga
     FROM meccsek m
     INNER JOIN csapatok c1 ON m.csapat1_id = c1.id
     INNER JOIN csapatok c2 ON m.csapat2_id = c2.id
