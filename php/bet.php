fe571e6d3b309c543085207eab87990f7cb64a05
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2026-02-12 07:54:29 +0100

    Bethez az adatok hozzáadasa az sqlbol html ujra csinalasa

--- php/bet.php
+++ php/bet.php
@@ -4,7 +4,7 @@ require_once('../../common/php/environment.php');
 // adatbázis kapcsolat
 $db = new Database('fotstats');
 
-// hírek lekérése
+// bet adatainak lekérese + a csapatok hozzáadása JOIN hozzá
 $rows = $db->execute(
   "SELECT
     m.id,
@@ -14,7 +14,15 @@ $rows = $db->execute(
     b.odd_dontetlen,
     b.odd_vendeg,
     b.odd_over_25,
-    b.odd_btts_igen
+    b.odd_over_15,
+    odd_under_15,
+    odd_under_25,
+    b.odd_btts_igen,
+    b.odd_btts_nem,
+    odd_szoglet_over_75,
+    odd_szoglet_under_75,
+    odd_tobb_szoglet_hazai,
+    odd_tobb_szoglet_vendeg
     FROM meccsek m
     JOIN csapatok ch ON m.csapat1_id = ch.id
     JOIN csapatok cv ON m.csapat2_id = cv.id
