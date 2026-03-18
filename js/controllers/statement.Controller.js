4fb6318c9d251bf35a0654e73c9dc1b930f99454
Ricsino7 <bokor.richard-2021@keri.mako.hu>
2026-03-18 09:12:38 +0100

    Hivatalos álláspont

--- js/controllers/statement.Controller.js
+++ js/controllers/statement.Controller.js
@@ -1,12 +1,18 @@
-;(function(window, angular) {
+;(function(window, angular){
   'use strict';
 
+  // Ellenőrzött modul, ha még nincs deklarálva
   angular.module('app')
-  .controller('rolunkController', [
-    '$scope',
-    function($scope) {
-        console.log("Statement controller..")
-    }
-  ]);
+    .controller('statementController', ['$scope', function($scope){
+      $scope.modalOpen = false;
+
+      $scope.openModal = function() {
+          $scope.modalOpen = true;
+      };
+
+      $scope.closeModal = function() {
+          $scope.modalOpen = false;
+      };
+  }]);
 
 })(window, angular);
